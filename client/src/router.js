37e941041d321cfad4bfe2ac15d6595111360a61
PK-1304 <pushkark1304@gmail.com>
2026-08-28 21:44:53 +0530

    defined routes

--- client/src/router.js
+++ client/src/router.js
@@ -1,10 +1,12 @@
 import { createElement } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import DashboardPage from './DashboardPage.jsx'
 import WorkflowsPage from './WorkflowsPage.jsx'
 
 const router = createBrowserRouter([
   { path: '/', element: createElement(App) },
+  { path: '/dashboard', element: createElement(DashboardPage) },
   { path: '/workflows', element: createElement(WorkflowsPage) },
 ])
 
