a2c3c5403bce676fbc5dfe1a6469717fbd55bcc8
PK-1304 <pushkark1304@gmail.com>
2026-09-13 18:35:05 +0530

    remove auth routes from router file

--- client/src/router.js
+++ client/src/router.js
@@ -6,15 +6,9 @@ import WorkflowsPage from './WorkflowsPage.jsx'
 import LandingPage from './LandingPage.jsx'
 import GraphViewPage from './graph/GraphViewPage.jsx'
 import WorkflowStatusPage from './WorkflowStatusPage.jsx'
-import SignupPage from './SignupPage.jsx'
-import LoginPage from './LoginPage.jsx'
-import PublicRoute from './components/PublicRoute.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 const router = createBrowserRouter([
   { path: '/', element: createElement(LandingPage) },
-  { path: '/auth/signup', element: createElement(SignupPage) },
-  { path: '/auth/login', element: createElement(LoginPage) },
   { path: '/dashboard', element: createElement(DashboardPage) },
   { path: '/workflows', element: createElement(WorkflowsPage) },
   { path: '/graph-view', element: createElement(GraphViewPage) },
