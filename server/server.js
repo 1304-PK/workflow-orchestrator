b410cd4ac8d37e47b2e1d62b90f9b9a4b189829d
PK-1304 <pushkark1304@gmail.com>
2026-09-13 18:36:56 +0530

    minor tweaks

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ require("dotenv").config()
 
 const app = express()
 
+
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
