5f853dc22d82c30d474a095e6aa179b24162d858
PK-1304 <pushkark1304@gmail.com>
2026-09-13 18:34:48 +0530

    remove auth context from main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,8 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.js'
 
-import { AuthProvider } from './context/AuthContext.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
